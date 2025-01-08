050316fc109bc4ed5c131c4aa102a631ee909355
Washington <washiem2022@gmail.com>
2025-01-08 20:36:30 +0300

    put back the headers for styling

--- views/register.php
+++ views/register.php
@@ -75,6 +75,20 @@ if(isset($_SESSION['user_id'])){
     }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <title>Registration</title>
+</head>
+<body class="grey lighten-4">
+<?php include '../config/header.php'; ?>
+</div>
+</nav>
+
 <section class="container grey-text">
     <h4 class="center">REGISTRATION</h4>
     <form class="white" action="register.php" method="POST">
